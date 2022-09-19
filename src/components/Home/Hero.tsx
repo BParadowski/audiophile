183b1e61bbf2c9798bd464e4d85c879b06935b5c
BParadowski <bartoszparadowski01@gmail.com>
2022-09-19 21:00:48 +0200

    Implement the hero component

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -8,7 +8,9 @@ const Hero = () => {
       <div className={styles["hero-container"]}>
         <div className={styles.grid}>
           <div className={styles.text}>
-            <p>
+            <p className={styles["new-product"]}>new product</p>
+            <h2>XX99 Mark II HeadphoneS</h2>
+            <p className={styles["hero-text"]}>
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
