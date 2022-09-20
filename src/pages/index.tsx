378c08bd8dcfcd3df14dcc8d0ba4c277acbe2aa5
BParadowski <bartoszparadowski01@gmail.com>
2022-09-20 16:26:32 +0200

    Implement home page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
 import Image from "next/future/image";
 import styles from "../styles/pages/Home.module.scss";
@@ -20,8 +21,58 @@ const Home: NextPage = () => {
         <div className={styles["categories-wrapper"]}>
           <ProductCategories />
         </div>
-
-        <Manifesto />
+        <section className={styles["products-wrapper"]}>
+          <div className={styles.grid}>
+            <div className={styles.big}>
+              <Picture
+                desktopUrl="/assets/home/desktop/image-speaker-zx9.png"
+                tabletUrl="/assets/home/tablet/image-speaker-zx9.png"
+                mobileUrl="/assets/home/mobile/image-speaker-zx9.png"
+                alt="A magnificent speaker"
+                className={styles.big_picture}
+              />
+              <h2 className={styles.big_heading}>ZX9 SPEAKER</h2>
+              <p>
+                Upgrade to premium speakers that are phenomenally built to
+                deliver truly remarkable sound.
+              </p>
+              <Link href="/">
+                <a className="button-neutral-dark">see product</a>
+              </Link>
+            </div>
+            <div className={styles.medium}>
+              <Picture
+                desktopUrl="/assets/home/desktop/image-speaker-zx7.jpg"
+                tabletUrl="/assets/home/tablet/image-speaker-zx7.jpg"
+                mobileUrl="/assets/home/mobile/image-speaker-zx7.jpg"
+                alt="A magnificent speaker"
+                className={styles.medium_picture}
+              />
+              <div className={styles.medium_wrapper}>
+                <h2 className={styles.medium_heading}>ZX7 SPEAKER</h2>
+                <Link href="/">
+                  <a className="button-neutral-light">see product</a>
+                </Link>
+              </div>
+            </div>
+            <Picture
+              desktopUrl="/assets/home/desktop/image-earphones-yx1.jpg"
+              tabletUrl="/assets/home/tablet/image-earphones-yx1.jpg"
+              mobileUrl="/assets/home/mobile/image-earphones-yx1.jpg"
+              alt="A pair of earphones, charging"
+              className={styles.image}
+            />
+            <div className={styles.small}>
+              <h2 className={styles.small_heading}>YX1 EARPHONES</h2>
+              <Link href="/">
+                <a className="button-neutral-light">see product</a>
+              </Link>
+            </div>
+          </div>
+        </section>
+        <div className={styles["manifesto-wrapper"]}>
+          <Manifesto />
+        </div>
       </div>
     </main>
   );
