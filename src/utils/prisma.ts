037745766633e2cbf6c0b9ec7d82c4a99cdfce5e
BParadowski <bartoszparadowski01@gmail.com>
2023-08-30 17:45:12 +0200

    Fixing more ts errors

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -5,9 +5,14 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
+  //@ts-expect-errorts-ignore
   if (!global.prisma) {
+    //@ts-expect-errorts-ignore
+
     global.prisma = new PrismaClient();
   }
+  //@ts-expect-errorts-ignore
+
   prisma = global.prisma;
 }
 
