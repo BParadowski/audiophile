e3f2b4cf1c5b63fdb60f2785692d120edb6a22ee
BParadowski <bartoszparadowski01@gmail.com>
2024-05-20 09:23:15 +0200

    Add fetching awareness to place order button

--- src/components/CheckoutPage/CheckoutForm/CheckoutFormSubmitButton.tsx
+++ src/components/CheckoutPage/CheckoutForm/CheckoutFormSubmitButton.tsx
@@ -2,23 +2,36 @@ import { formHTMLId } from "./formConfig";
 
 import { useFormContext } from "react-hook-form";
 
+import { useCart } from "@/components/Cart/useCart";
+
 const CheckoutFormSubmitButton = () => {
   const {
     formState: { isSubmitting },
     watch,
   } = useFormContext();
+  const cart = useCart();
 
   const selectedPaymentMethod = watch("paymentMethod");
 
-  return isSubmitting ? (
-    <button className="button-accent" type="button">
-      Placing order...
-    </button>
-  ) : (
-    <button className="button-accent" type="submit" form={formHTMLId}>
-      {selectedPaymentMethod === "card" ? "continue & pay" : "continue"}
-    </button>
-  );
+  if (cart.isFetching) {
+    return (
+      <button className="button-accent" type="button">
+        Loading cart...
+      </button>
+    );
+  } else if (isSubmitting) {
+    return (
+      <button className="button-accent" type="button">
+        Placing order...
+      </button>
+    );
+  } else {
+    return (
+      <button className="button-accent" type="submit" form={formHTMLId}>
+        {selectedPaymentMethod === "card" ? "continue & pay" : "continue"}
+      </button>
+    );
+  }
 };
 
 export default CheckoutFormSubmitButton;
