668ca50a9b626a11d5ed0a38a37cb41b12e4e332
BParadowski <bartoszparadowski01@gmail.com>
2024-05-21 12:21:37 +0200

    Remove bad global styles

--- src/pages/api/clear-cart.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-import prisma from "@/utils/backend/prisma";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  /* Nextjs automaticaly parses the request body based on headers*/
-  const { cartId } = req.body;
-  console.log(cartId);
-}
